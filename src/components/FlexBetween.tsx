b530607709ffa8814bd8c137072409e27da1f274
AlexMagar <lx.mgr07@gmail.com>
2023-11-28 22:04:30 +1100

    new: flex component

---
+++ src/components/FlexBetween.tsx
@@ -0,0 +1,11 @@
+import { Box, styled } from "@mui/material";
+
+
+//syntax for styling the basic componenets 
+const FlexBetween = styled(Box)({
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center",
+})
+
+export default FlexBetween
