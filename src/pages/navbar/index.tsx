062fa9a720ea70c861de1c35153d00102aa1134b
AlexMagar <lx.mgr07@gmail.com>
2023-11-29 22:19:43 +1100

    update: header

--- src/pages/navbar/index.tsx
+++ src/pages/navbar/index.tsx
@@ -1,19 +1,53 @@
 import FlexBetween from "@/components/FlexBetween"
-import { useTheme } from "@mui/material"
+import { Box, Link, Typography, useTheme } from "@mui/material"
 import { useState } from "react"
+import PixIcon from '@mui/icons-material/Pix';
 
 type Props = {}
 
 const Navbar = (props: Props) => {
 
     const { palette } = useTheme()
-    const [selected, setSelected] = useState("dashboard")
+    const [selected, setSelected] = useState("dashboard") //state what page we are on
 
     const content = (
-        <FlexBetween
-        mb="o.25rem" p="o.5rem 0rem"
-        color={palette.grey[300]}
-        >Index file</ FlexBetween>
+        <FlexBetween mb="o.25rem" p="o.5rem 0rem" color={palette.grey[300]} >
+          {/* ========== Left Side ========== */}
+          <FlexBetween gap="0.75rem">
+            <PixIcon sx={{fontSize: "28px"}}/>
+            <Typography variant="h4" fontSize="16px">
+              Finanseer
+            </Typography>
+          </FlexBetween>
+
+          {/* =============== Right Side =============  */}
+          <FlexBetween gap="2rem">
+            <Box sx={{"&:hover": {color: palette.primary[100]}}}>
+              <Link 
+                to="/" 
+                onClick ={() => setSelected("dashboard")}
+                style={{
+                  color: selected === "dashboard" ? "inherit" : palette.grey[700],
+                  textDecoration: "inherit"
+                }}
+              >
+                Dashboard
+              </Link>
+            </Box>
+            <Box sx={{"&:hover": {color: palette.primary[100]}}}>
+              <Link 
+                to="/predictions" 
+                onClick ={() => setSelected("predictions")}
+                style={{
+                  color: selected === "predictions" ? "inherit" : palette.grey[700],
+                  textDecoration: "inherit"
+                }}
+              >
+                predictions
+              </Link>
+            </Box>
+          </FlexBetween>
+        </ FlexBetween>
     )
 
   return content
