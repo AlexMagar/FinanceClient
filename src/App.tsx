40baef3f940574ae1dd5ff39ddb0f15b581ea3fe
AlexMagar <lx.mgr07@gmail.com>
2023-11-29 22:11:38 +1100

    comp: Vomponenetn, dashboardm amdprecvtion

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { useMemo } from 'react'
 import { themeSettings } from './theme'
 import { Box, CssBaseline } from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from '@/pages/navbar'
+import Navbar from '@/pages/navbar/index'
+import Dashboard from "@/pages/dashboard";
 
 function App() {
 
@@ -17,8 +18,8 @@ function App() {
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
           <Navbar />
           <Routes>
-            <Route path='/' element={<div>Dashboard</div>}/>
-            <Route path='/prediction' element={<div>Prediction Page</div>}/>
+            <Route path='/' element={<Dashboard />}/>
+            <Route path='/prediction' element={<div>predictions Page</div>}/>
           </Routes>
         </Box>
       </ThemeProvider>
