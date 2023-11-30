84a0d64009591e64a439f8d60bf9a5ff705173e9
AlexMagar <lx.mgr07@gmail.com>
2023-11-30 23:21:34 +1100

    feat: grid layout of the boxes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,84 @@
-import { useTheme } from '@emotion/react'
-import { Box } from '@mui/material'
-import React from 'react'
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box, useMediaQuery } from '@mui/material'
+import Row1 from './Row1'
+import Row2 from './Row2'
+import Row3 from './Row3'
 
 type Props = {}
 
-const Dashboard = (props: Props) => {
-    const { palette } = useTheme()
-  return (
-    <Box color={palette.gray[300]}>Dashboard</Box>
-  )
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+  `
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+  `
+
+const Dashboard = (_props: Props) => {
+
+  const isAboveMediumScreen =useMediaQuery("(min-width: 1200px)")
+
+    const content = (
+      <Box width="100%" height="100%" display="grid" gap="1.5rem"
+        sx={
+          isAboveMediumScreen 
+          ? {
+          gridTemplateColumns: " repeat(3, minmax(370px, 1fr))", //3 columns of min-370px
+          gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+          gridTemplateAreas: gridTemplateLargeScreens,
+          }
+        : {
+          gridAutoColumns: "1fr",
+          gridAutoRows: "80px",
+          gridTemplateAreas : gridTemplateSmallScreens,
+          }
+      }
+      >
+        <Row1 />
+        <Row2 />
+        <Row3 />
+      </Box>
+    )
+  return content
 }
 
 export default Dashboard
