b31f5f382e1cbeb150df49471e1ca65f5f516052
AlexMagar <lx.mgr07@gmail.com>
2023-11-30 23:22:52 +1100

    feat: seprating 3rd row from Dasboard index itself

---
+++ src/pages/dashboard/Row3.tsx
@@ -0,0 +1,15 @@
+import DashboardBox from '@/components/DashboardBox'
+
+const Row3 = () => {
+  
+    const content = (
+        <>
+            <DashboardBox gridArea="h">h</DashboardBox>
+            <DashboardBox gridArea="i">i</DashboardBox>
+            <DashboardBox gridArea="j">j</DashboardBox>
+        </>
+    )
+  return content
+}
+
+export default Row3
