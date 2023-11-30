16d23378077791b79aaf84426e87673fc596ef15
AlexMagar <lx.mgr07@gmail.com>
2023-11-30 23:22:40 +1100

    feat: seprating 2nd row from Dasboard index itself

---
+++ src/pages/dashboard/Row2.tsx
@@ -0,0 +1,16 @@
+import DashboardBox from '@/components/DashboardBox'
+
+
+const Row2 = () => {
+  
+    const content = (
+        <>
+            <DashboardBox gridArea="e">e</DashboardBox>
+            <DashboardBox gridArea="f">f</DashboardBox>
+            <DashboardBox gridArea="g">g</DashboardBox>
+        </>
+    )
+  return content
+}
+
+export default Row2
