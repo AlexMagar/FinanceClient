f4e296a7e32a5bc60eea53cf1bd00816dfb116a4
AlexMagar <lx.mgr07@gmail.com>
2023-12-01 23:23:41 +1100

    style: spacing and console log

--- src/pages/dashboard/Row1.tsx
+++ src/pages/dashboard/Row1.tsx
@@ -1,8 +1,10 @@
 import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery } from '@/state/api'
 
 const Row1 = () => {
 
-    // const { data } = useGetKpisQuery()
+    const { data } = useGetKpisQuery()
+    console.log(data)
 
     const content = (
         <>
