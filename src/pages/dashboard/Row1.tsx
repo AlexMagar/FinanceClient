af188738278cdc24bcfded7777713004c7bccb93
AlexMagar <lx.mgr07@gmail.com>
2023-12-09 23:29:09 +1100

    feat: new two verticle sheet

--- src/pages/dashboard/Row1.tsx
+++ src/pages/dashboard/Row1.tsx
@@ -3,7 +3,7 @@ import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
 import { useTheme } from '@mui/material'
 import { useMemo } from 'react'
-import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Area, AreaChart, CartesianGrid, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 const Row1 = () => {
     const { palette } = useTheme();
@@ -92,7 +92,90 @@ const Row1 = () => {
                 </AreaChart>
             </ResponsiveContainer>
             </DashboardBox>
-            <DashboardBox gridArea="b">b</DashboardBox>
+            <DashboardBox gridArea="b">
+
+            <BoxHeader
+                title='Revenue and Expenses'
+                subtitle='Top line representes revenue, bottom line represent'
+                sideText='+4%'
+            />
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={revenueExpenses}
+                    margin={{
+                        top: 15,
+                        right: 25,
+                        left: -10,
+                        bottom: 60,
+                    }}
+                >
+                    <CartesianGrid 
+                        vertical={false}
+                        stroke={palette.grey[800]}
+                    />
+                    <defs>
+                        <linearGradient id='colorRevenue' x1="0" y1="0" x2="0" y2="1">
+                            <stop 
+                                offset="5%"
+                                stopColor={palette.primary[300]}
+                                stopOpacity={0.5}
+                            />
+                            <stop 
+                                offset="95%"
+                                stopColor={palette.primary[300]}
+                                stopOpacity={0}
+                            />
+                        </linearGradient>
+                        <linearGradient id='colorExpenses' x1="0" y1="0" x2="0" y2="1">
+                            <stop 
+                                offset="5%"
+                                stopColor={palette.primary[300]}
+                                stopOpacity={0.5}
+                            />
+                            <stop 
+                                offset="95%"
+                                stopColor={palette.primary[300]}
+                                stopOpacity={0}
+                            />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="name"/>
+                    <YAxis 
+                        yAxisId="left"
+                        orientation='right'
+                        tickLine={false}
+                        axisLine={false}
+                        style={{ fontSize: "10px"}}
+                    />
+                    <YAxis 
+                        yAxisId="right"
+                        orientation='right'
+                        tickLine={false}
+                        axisLine={false}
+                        style={{ fontSize: "10px"}}
+                    />
+                    <Tooltip />
+                    <Line 
+                        yAxisId="left"
+                        type="monotone" 
+                        dataKey="profilt" 
+                        stroke={palette.primary.main}
+                        fillOpacity={1}
+                        fill="url(#colorRevenue)"
+                    />
+                    <Line
+                        type="monotone" 
+                        dataKey="expenses" 
+                        dot={true}
+                        stroke={palette.tertiary[500]}
+                        fillOpacity={1}
+                        fill="url(#colorExpenses)"
+                    />
+                </AreaChart>
+            </ResponsiveContainer>
+            </DashboardBox>
             <DashboardBox gridArea="c">c</DashboardBox>
             <DashboardBox gridArea="d">d</DashboardBox>
         </>
