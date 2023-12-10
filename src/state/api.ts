f7c7f7c032f9370253d95a1637a35712f0277667
AlexMagar <lx.mgr07@gmail.com>
2023-12-10 22:38:32 +1100

    feat: product in redux

--- src/state/api.ts
+++ src/state/api.ts
@@ -4,12 +4,16 @@ import { GetKpiResponsive } from "./types";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ['Kpis'],
+    tagTypes: ['Kpis', 'Products'],
     endpoints: (build) => ({
         getKpis : build.query<Array<GetKpiResponsive>, void>({
             query: () => "kpi/kpis/",
             providesTags: ['Kpis']
-        })
+        }),
+        getProducts : build.query<Array<GetKpiResponsive>, void>({
+            query: () => "product/products/",
+            providesTags: ['Products'],
+        }),
     })
 })
 
