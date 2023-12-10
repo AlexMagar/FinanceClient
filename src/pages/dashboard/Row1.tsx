f61681a092f544c625b88daf6507fc11c3a5b167
AlexMagar <lx.mgr07@gmail.com>
2023-12-10 22:37:55 +1100

    feat: new graph

--- src/pages/dashboard/Row1.tsx
+++ src/pages/dashboard/Row1.tsx
@@ -3,7 +3,7 @@ import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
 import { useTheme } from '@mui/material'
 import { useMemo } from 'react'
-import { Area, AreaChart, CartesianGrid, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 const Row1 = () => {
     const { palette } = useTheme();
@@ -18,6 +18,31 @@ const Row1 = () => {
                     name: month.substring(0, 3),
                     revenue: revenue,
                     expenses: expenses,
+                }
+            })
+        )
+    }, [data])
+
+    const revenueProfilt = useMemo( () => {
+        return(
+            data &&
+            data[0].monthlyData.map(({month, revenue, expenses}) => {
+                return {
+                    name: month.substring(0, 3),
+                    revenue: revenue,
+                    profilt: (revenue - expenses).toFixed(2),
+                }
+            })
+        )
+    }, [data])
+
+    const revenue = useMemo( () => {
+        return(
+            data &&
+            data[0].monthlyData.map(({month, revenue}) => {
+                return {
+                    name: month.substring(0, 3),
+                    revenue: revenue,
                 }
             })
         )
@@ -92,59 +117,36 @@ const Row1 = () => {
                 </AreaChart>
             </ResponsiveContainer>
             </DashboardBox>
-            <DashboardBox gridArea="b">
 
+            <DashboardBox gridArea="b">
             <BoxHeader
-                title='Revenue and Expenses'
+                title='Profilt and Revenue'
                 subtitle='Top line representes revenue, bottom line represent'
                 sideText='+4%'
             />
             <ResponsiveContainer width="100%" height="100%">
-                <AreaChart
+                <LineChart
                     width={500}
                     height={400}
-                    data={revenueExpenses}
+                    data={revenueProfilt}
                     margin={{
-                        top: 15,
-                        right: 25,
+                        top: 20,
+                        right: 0,
                         left: -10,
-                        bottom: 60,
+                        bottom: 55,
                     }}
                 >
                     <CartesianGrid 
                         vertical={false}
                         stroke={palette.grey[800]}
                     />
-                    <defs>
-                        <linearGradient id='colorRevenue' x1="0" y1="0" x2="0" y2="1">
-                            <stop 
-                                offset="5%"
-                                stopColor={palette.primary[300]}
-                                stopOpacity={0.5}
-                            />
-                            <stop 
-                                offset="95%"
-                                stopColor={palette.primary[300]}
-                                stopOpacity={0}
-                            />
-                        </linearGradient>
-                        <linearGradient id='colorExpenses' x1="0" y1="0" x2="0" y2="1">
-                            <stop 
-                                offset="5%"
-                                stopColor={palette.primary[300]}
-                                stopOpacity={0.5}
-                            />
-                            <stop 
-                                offset="95%"
-                                stopColor={palette.primary[300]}
-                                stopOpacity={0}
-                            />
-                        </linearGradient>
-                    </defs>
-                    <XAxis dataKey="name"/>
+                    <XAxis 
+                        dataKey="name"
+                        tickLine={false}
+                        style={{fontSize: "10px"}}
+                    />
                     <YAxis 
                         yAxisId="left"
-                        orientation='right'
                         tickLine={false}
                         axisLine={false}
                         style={{ fontSize: "10px"}}
@@ -157,27 +159,80 @@ const Row1 = () => {
                         style={{ fontSize: "10px"}}
                     />
                     <Tooltip />
+                    <Legend 
+                        height={20} 
+                        wrapperStyle={{
+                        margin: " 0 0 10px 0"
+                    }}/>
                     <Line 
                         yAxisId="left"
                         type="monotone" 
                         dataKey="profilt" 
-                        stroke={palette.primary.main}
-                        fillOpacity={1}
-                        fill="url(#colorRevenue)"
+                        stroke={palette.tertiary[500]}
                     />
                     <Line
+                        yAxisId="right"
                         type="monotone" 
-                        dataKey="expenses" 
-                        dot={true}
-                        stroke={palette.tertiary[500]}
-                        fillOpacity={1}
-                        fill="url(#colorExpenses)"
+                        dataKey="revenue" 
+                        stroke={palette.primary.main}
                     />
-                </AreaChart>
+                </LineChart>
             </ResponsiveContainer>
             </DashboardBox>
-            <DashboardBox gridArea="c">c</DashboardBox>
-            <DashboardBox gridArea="d">d</DashboardBox>
+
+            <DashboardBox gridArea="c">
+                <BoxHeader
+                    title='Revenue Month by Month'
+                    subtitle='Graph representing the revenue month by month'
+                    sideText='+4%'
+                />
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={revenue}
+                        margin={{
+                          top: 17,
+                          right: 15,
+                          left: -5,
+                          bottom: 58,
+                    }}>
+                    <defs>
+                        <linearGradient id='colorRevenue' x1="0" y1="0" x2="0" y2="1">
+                            <stop 
+                                offset="5%"
+                                stopColor={palette.primary[300]}
+                                stopOpacity={0.8}
+                            />
+                            <stop 
+                                offset="95%"
+                                stopColor={palette.primary[300]}
+                                stopOpacity={0}
+                            />
+                        </linearGradient>
+
+                    </defs>
+                    <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
+                    <XAxis 
+                        dataKey="name" 
+                        axisLine={false} 
+                        tickLine={false} 
+                        style={{fontSize: '10px'}}
+                    />
+                    <YAxis 
+                        axisLine={false} 
+                        tickLine={false} 
+                        style={{fontSize: '10px'}}
+                    />
+                    <Tooltip />
+                    <Bar 
+                        dataKey="revenue" 
+                        fill="url(#colorRevenue)" 
+                        activeBar={<Rectangle fill="pink" stroke="blue" />} 
+                    />
+                  </BarChart>
+                </ResponsiveContainer>
+            </DashboardBox>
         </>
     )
   return content
