e76bb0b8da094a4c05a0d07347a43a4edb996d5b
AlexMagar <lx.mgr07@gmail.com>
2023-12-11 23:24:28 +1100

    chore: bug fixed

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,20 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpiResponsive } from "./types";
+import { GetKpiResponse, GetProductsResponse } from "./types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
     tagTypes: ['Kpis', 'Products'],
     endpoints: (build) => ({
-        getKpis : build.query<Array<GetKpiResponsive>, void>({
+        getKpis : build.query<Array<GetKpiResponse>, void>({
             query: () => "kpi/kpis/",
             providesTags: ['Kpis']
         }),
-        getProducts : build.query<Array<GetKpiResponsive>, void>({
+        getProducts : build.query<Array<GetProductsResponse>, void>({
             query: () => "product/products/",
             providesTags: ['Products'],
         }),
     })
 })
 
-export const { useGetKpisQuery } = api
+export const { useGetKpisQuery, useGetProductsQuery } = api
