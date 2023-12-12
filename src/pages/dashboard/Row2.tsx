6d808fd1c7e32a7a90d9dab38c1f1c27a83d7af2
AlexMagar <lx.mgr07@gmail.com>
2023-12-12 23:21:07 +1100

    feat: pie chart representating campaings and targets

--- src/pages/dashboard/Row2.tsx
+++ src/pages/dashboard/Row2.tsx
@@ -1,13 +1,19 @@
 import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox'
+import FlexBetween from '@/components/FlexBetween';
 import { useGetKpisQuery, useGetProductsQuery } from '@/state/api'
-import { useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { useMemo } from 'react';
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Cell, Line, LineChart, Pie, PieChart, ResponsiveContainer, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from 'recharts';
 
+const pieData = [
+    { name: "Group A", value: 600 },
+    { name: "Group B", value: 400},
+]
 
 const Row2 = () => {
     const { palette } = useTheme()
+    const pieColors = [palette.primary[800], palette.primary[300]]
     const {data: productData} = useGetProductsQuery();
     const { data: operationalData } = useGetKpisQuery()
     console.log(productData)
@@ -81,8 +87,83 @@ const Row2 = () => {
                     </LineChart>
                 </ResponsiveContainer>
             </DashboardBox>
-            <DashboardBox gridArea="e">e</DashboardBox>
-            <DashboardBox gridArea="f">f</DashboardBox>
+            <DashboardBox gridArea="e">
+                <BoxHeader 
+                    title='Campaigns and Targets'
+                    sideText='+4%'
+                />
+                <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+                    <PieChart 
+                        width={110}
+                        height={100}
+                        margin={{
+                            top: 0,
+                            right: -10,
+                            bottom: 0,
+                            left: 10,
+                        }}
+                    >
+                        <Pie
+                            stroke='none'
+                            data={pieData}
+                            innerRadius={10}
+                            outerRadius={38}
+                            paddingAngle={2}
+                            dataKey="value"
+                        >
+                            {pieData.map((entry, index) => (
+                                <Cell 
+                                    key={`cell-${index}`} 
+                                    fill={pieColors[index]}
+                                />
+                            ))}
+                        </Pie>
+                    </PieChart>
+                    <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+                        <Typography variant='h5'>Sales</Typography>
+                        <Typography m='0.3rem 0' variant='h3' color={palette.primary[300]}>83</Typography>
+                        <Typography variant='h6'>Finance goals of the campaign that is desired</Typography>
+                    </Box>
+                    <Box flexBasis="40%">
+                        <Typography variant='h5'>Losses in Revenue</Typography>
+                        <Typography variant='h6'>Losses are down 25%</Typography>
+                        <Typography mt='0.4rem' variant='h5'>Profile margin</Typography>
+                        <Typography variant='h6'>Margins are up by 30% from last month</Typography>
+                    </Box>
+                </FlexBetween>
+            </DashboardBox>
+            <DashboardBox gridArea="f">
+                <BoxHeader 
+                    title='Product Prices vs Expenses'
+                    sideText='+4%'
+                />
+                <ResponsiveContainer width="100%" height={400}>
+                  <ScatterChart
+                    margin={{
+                      top: 20,
+                      right: 20,
+                      bottom: 20,
+                      left: 20,
+                    }}
+                  >
+                    <CartesianGrid />
+                    <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+                    <YAxis yAxisId="left" type="number" dataKey="y" name="weight" unit="kg" stroke="#8884d8" />
+                    <YAxis
+                      yAxisId="right"
+                      type="number"
+                      dataKey="y"
+                      name="weight"
+                      unit="kg"
+                      orientation="right"
+                      stroke="#82ca9d"
+                    />
+                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                    <Scatter yAxisId="left" name="A school" data={data01} fill="#8884d8" />
+                    <Scatter yAxisId="right" name="A school" data={data02} fill="#82ca9d" />
+                  </ScatterChart>
+                </ResponsiveContainer>
+            </DashboardBox>
         </>
     )
   return content
