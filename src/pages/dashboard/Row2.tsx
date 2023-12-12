feb020916dffd8dfae8b41647a12216c6299bf41
AlexMagar <lx.mgr07@gmail.com>
2023-12-12 22:20:24 +1100

    feat: updated product and operational data

--- src/pages/dashboard/Row2.tsx
+++ src/pages/dashboard/Row2.tsx
@@ -1,26 +1,40 @@
 import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox'
-import { useGetProductsQuery } from '@/state/api'
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { useGetKpisQuery, useGetProductsQuery } from '@/state/api'
+import { useTheme } from '@mui/material';
+import { useMemo } from 'react';
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 
 const Row2 = () => {
+    const { palette } = useTheme()
+    const {data: productData} = useGetProductsQuery();
+    const { data: operationalData } = useGetKpisQuery()
+    console.log(productData)
 
-    const {data} = useGetProductsQuery();
-    console.log(data)
+    const operationalExpenses = useMemo( () => {
+        return(
+            operationalData &&
+            operationalData[0].monthlyData.map(({month, operationalExpenses, nonOperationalExpenses}) => {
+                return {
+                    name: month.substring(0, 3),
+                    "Operational Expenses": operationalExpenses,
+                    "Non operational Expenses": nonOperationalExpenses,
+                }
+            })
+        )
+    }, [operationalData])
     const content = (
         <>
             <DashboardBox gridArea="d">
                 <BoxHeader
-                    title='Profilt and Revenue'
-                    subtitle='Top line representes revenue, bottom line represent'
+                    title='Operational vs Non-Operatioinal Expenses'
+                    // subtitle='Top line representes revenue, bottom line represent'
                     sideText='+4%'
                 />
                 <ResponsiveContainer width="100%" height="100%">
                     <LineChart
-                        width={500}
-                        height={400}
-                        data={revenueProfilt}
+                        data={operationalExpenses}
                         margin={{
                             top: 20,
                             right: 0,
@@ -39,6 +53,7 @@ const Row2 = () => {
                         />
                         <YAxis 
                             yAxisId="left"
+                            orientation='left'
                             tickLine={false}
                             axisLine={false}
                             style={{ fontSize: "10px"}}
@@ -51,28 +66,23 @@ const Row2 = () => {
                             style={{ fontSize: "10px"}}
                         />
                         <Tooltip />
-                        <Legend 
-                            height={20} 
-                            wrapperStyle={{
-                            margin: " 0 0 10px 0"
-                        }}/>
                         <Line 
                             yAxisId="left"
                             type="monotone" 
-                            dataKey="profilt" 
+                            dataKey="Non Operational Expenses" 
                             stroke={palette.tertiary[500]}
                         />
                         <Line
                             yAxisId="right"
                             type="monotone" 
-                            dataKey="revenue" 
+                            dataKey="Operational Expenses" 
                             stroke={palette.primary.main}
                         />
                     </LineChart>
                 </ResponsiveContainer>
             </DashboardBox>
+            <DashboardBox gridArea="e">e</DashboardBox>
             <DashboardBox gridArea="f">f</DashboardBox>
-            <DashboardBox gridArea="g">g</DashboardBox>
         </>
     )
   return content
