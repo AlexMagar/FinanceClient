b30876b232375e8f7c0e96e8c08090f2422d2212
AlexMagar <lx.mgr07@gmail.com>
2023-12-14 21:58:54 +1100

    chore: debug

--- src/pages/dashboard/Row3.tsx
+++ src/pages/dashboard/Row3.tsx
@@ -1,13 +1,38 @@
+import BoxHeader from '@/components/BoxHeader'
 import DashboardBox from '@/components/DashboardBox'
 
 const Row3 = () => {
   
     const content = (
         <>
-            <DashboardBox gridArea="g">g</DashboardBox>
-            <DashboardBox gridArea="h">h</DashboardBox>
-            <DashboardBox gridArea="i">i</DashboardBox>
-            <DashboardBox gridArea="j">j</DashboardBox>
+            <DashboardBox gridArea="g">
+                <BoxHeader
+                    title='List of Products'
+                    // subtitle='Top line representes revenue, bottom line represent'
+                    sideText='124 Products'
+                />
+            </DashboardBox>
+            <DashboardBox gridArea="h">
+                <BoxHeader
+                    title='Recent Orders'
+                    // subtitle='Top line representes revenue, bottom line represent'
+                    sideText='50 latest transactions'
+                />
+            </DashboardBox>
+            <DashboardBox gridArea="i">
+                <BoxHeader
+                    title='Expense Breakdown by Category'
+                    // subtitle='Top line representes revenue, bottom line represent'
+                    sideText='+4%'
+                />
+            </DashboardBox>
+            <DashboardBox gridArea="j">
+                <BoxHeader
+                    title='Overall Summary and Explanation Data'
+                    // subtitle='Top line representes revenue, bottom line represent'
+                    sideText='+15%'
+                />
+            </DashboardBox>
         </>
     )
   return content
