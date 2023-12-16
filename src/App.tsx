84bcbf0a19ab74ef6b4f025de683efcc11433714
AlexMagar <lx.mgr07@gmail.com>
2023-12-16 22:54:40 +1100

    feat: = dashboard and prediction routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { themeSettings } from './theme'
 import { Box, CssBaseline } from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '@/pages/navbar/index'
-import Dashboard from "@/pages/dashboard";
-import Prediction from './pages/predection'
+import Dashboard from './pages/dashboard'
+import Predictions from './pages/predictions'
 
 function App() {
 
@@ -19,8 +19,8 @@ function App() {
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
           <Navbar />
           <Routes>
-            <Route path='/' element={<Dashboard />}/>
-            <Route path='/predictions' element={<Prediction />}/>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/predictions' element={<Predictions />}/>
           </Routes>
         </Box>
       </ThemeProvider>
