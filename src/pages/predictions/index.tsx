154f12ac741c5363d04deb088ef4573ce067afbe
AlexMagar <lx.mgr07@gmail.com>
2023-12-17 21:42:29 +1100

    feat: regression line built

--- src/pages/predictions/index.tsx
+++ src/pages/predictions/index.tsx
@@ -2,14 +2,36 @@ import DashboardBox from '@/components/DashboardBox'
 import FlexBetween from '@/components/FlexBetween'
 import { useGetKpisQuery } from '@/state/api'
 import { Box, Button, Typography, useTheme } from '@mui/material'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { CartesianGrid, Label, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
-
+import regression, { DataPoint } from "regression";
 
 const Predictions = () => {
     const { palette } = useTheme()
     const [ isPredictions, setIsPredictions] = useState(false)
     const { data: kpiData } = useGetKpisQuery()
+
+    const formatedData = useMemo(() => {
+        if(!kpiData) return []
+
+        const monthData = kpiData[0].monthlyData;
+
+        const formatted: Array<DataPoint> = monthData.map(
+            ({ revenue }, i: number) => {
+                return [i, revenue]
+            }
+        )
+        const regressionLine = regression.linear(formatted)
+
+        return monthData.map(({month, revenue}, i: number) => {
+            return {
+                name: month,
+                "Actual Revenue": revenue,
+                "Regression Line": regressionLine.points[i][1],
+                "Predicted Revenue": regressionLine.predict(i + 12)[1],
+            }
+        })
+    }, [kpiData])
   return (
     <DashboardBox
         width="100%"
@@ -56,33 +78,35 @@ const Predictions = () => {
                 <YAxis 
                     domain={[12000, 26000]}
                     axisLine={{strokeWidth: "0"}}
-                    tickLine={false}
-                    axisLine={false}
                     style={{ fontSize: "10px"}}
+                    tickFormatter={(v) => `$${v}`}
                 >
                     <Label value="Revenue in USD" angle={-90} offset={-5} position="insideLeft" />   
                 </YAxis>
-                <YAxis 
-                    yAxisId="right"
-                    orientation='right'
-                    tickLine={false}
-                    axisLine={false}
-                    style={{ fontSize: "10px"}}
-                />
                 <Tooltip />
                 <Legend verticalAlign='top'/>
                 <Line
-                    yAxisId="left"
                     type="monotone" 
-                    dataKey="profilt" 
-                    stroke={palette.tertiary[500]}
+                    dataKey="Actual Revenue" 
+                    stroke={palette.primary.main}
+                    strokeWidth={0}
+                    dot={{strokeWidth: 5}}
                 />
                 <Line
-                    yAxisId="right"
                     type="monotone" 
-                    dataKey="revenue" 
-                    stroke={palette.primary.main}
+                    dataKey="Regression Line" 
+                    stroke="#8884d8"
+                    dot={false}
                 />
+                {
+                    isPredictions && (
+                        <Line
+                            strokeDasharray="5 5"
+                            dataKey="Regression Revenue" 
+                            stroke={palette.secondary[500]}
+                        />
+                    )
+                }
             </LineChart>
         </ResponsiveContainer>
     </DashboardBox>
