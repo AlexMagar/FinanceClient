24b9e448e15076ba8e8cde6633b4f3aa7c42b2c9
AlexMagar <lx.mgr07@gmail.com>
2023-12-24 15:24:46 +1100

    chore: typo

--- src/state/api.ts
+++ src/state/api.ts
@@ -15,7 +15,7 @@ export const api = createApi({
             providesTags: ['Products'],
         }),
         getTransactions : build.query<Array<GetTransactionsResponse>, void>({
-            query: () => "transaction/transactions",
+            query: () => "transaction/transactions/",
             providesTags: ['Transactions'],
         }),
     })
